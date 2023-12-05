95cb58ef94c28c7b9d5e42076dfd627c454dbb02
leo@tower <leonard.huth@googlemail.com>
2023-12-05 01:53:40 +0100

    some progress on day1 part2

--- src/day1/main.ts
+++ src/day1/main.ts
@@ -22,6 +22,40 @@ const extractNum = (input: string[]): string[][] => {
     })
 }
 
+const extractNumFromStr = (input: string[]): number[][] => {
+    const re: RegExp = new RegExp('one|two|three|four|five|six|seve|eight|nine', 'gm')
+    return input.map((item: string) => {
+        const matchArr :RegExpMatchArray | null= re[Symbol.match](item)
+        if(matchArr){
+            return matchArr.map((item: string) => {
+                switch (item){
+                    case 'one':
+                        return 1
+                    case 'two':
+                        return 2
+                    case 'three':
+                        return 3
+                    case 'four':
+                        return 4
+                    case 'five':
+                        return 5
+                    case 'six':
+                        return 6
+                    case 'seven':
+                        return 7
+                    case 'eight':
+                        return 8
+                    case 'nine':
+                        return 9
+                    default:
+                        return 0
+                }
+            })
+        }
+        return []
+    })
+}
+
 const convertToNum = (input: string[][]): number[][] => {
     return input.map((item: string[]) => {
         return item.map((item: string) => {
@@ -42,9 +76,11 @@ const sumItUp = (input: number[][]): number => {
 export const day1 = async () => {
     const result: string = await loadFile('src/day1/data.txt')
     const rowArr: string[] = convertToArr(result)
-    const test = extractNum(rowArr)
+    const lol = extractNumFromStr(rowArr)
+    console.log(lol)
+/*    const test = extractNum(rowArr)
     const test2 = convertToNum(test)
     const test3 = sumItUp(test2)
-    console.log(test3)
+    console.log(test3)*/
 }
 
